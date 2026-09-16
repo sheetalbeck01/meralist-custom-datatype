3575f4646c83e8fedda2035a4128304a4edb1f38
sheetalbeck01 <sheetalbeck01@gmail.com>
2026-09-16 18:29:35 +0530

    create MeraList class

---
+++ core.py
@@ -0,0 +1,18 @@
+import ctypes
+
+class MeraList:
+    def __init__(self):
+        self.size = 1
+        self.n = 0
+
+        # create a C type arrary with size = self.size
+        self.A = self.__make_array(self.size)
+
+
+    def __make_array(self, capacity):
+        # this creates c type static & referential array with size capacity
+        return (capacity*ctypes.py_object)()
+
+L = MeraList()
+
+print(type(L))
