dac9cf4d73493382312d5b3edfff1d4d7aea3274
sheetalbeck01 <sheetalbeck01@gmail.com>
2026-09-17 10:52:11 +0530

    feat/append

--- core.py
+++ core.py
@@ -11,11 +11,36 @@ class MeraList:
     def __len__(self):
         return self.n
 
+    def append(self, item):
+        if self.n == self.size: # check if array is empty or not
+            # resize
+            self.__resize(self.size*2)
+        
+        # append
+        self.A[self.n] = item
+        self.n = self.n + 1
+
+    def __resize(self, new_capacity):
+        # create a new array with new capacity
+        B = self.__make_array(new_capacity) # B is an array double the size of A
+        self.size = new_capacity
+
+        # copy the content of A to B
+        for i in range(self.n):
+            B[i] = self.A[i]
+
+        # reassign A because all the logic are written in A.
+        self.A = B
 
     def __make_array(self, capacity):
         # this creates c type static & referential array with size capacity
         return (capacity*ctypes.py_object)()
 
 L = MeraList()
+
+# test append
+L.append('Hello')
+L.append(2)
+L.append(True)
 
 print(type(L), len(L))
