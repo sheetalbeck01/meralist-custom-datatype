9000ff6d49c9a15924361527e2a121e50bffac5b
sheetalbeck01 <sheetalbeck01@gmail.com>
2026-09-18 15:30:51 +0530

    feat/pop

--- core.py
+++ core.py
@@ -35,6 +35,14 @@ class MeraList:
         self.A[self.n] = item
         self.n = self.n + 1
 
+    def pop(self):
+        if self.n == 0:
+            return 'Empty list'
+
+        print(self.A[self.n-1])
+        self.n = self.n -1
+
+
     def __resize(self, new_capacity):
         # create a new array with new capacity
         B = self.__make_array(new_capacity) # B is an array double the size of A
